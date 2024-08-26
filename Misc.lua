fdbd124536f288ca065ec0896a7cfb787391686f
toonmaterial <30956192+toonmaterial@users.noreply.github.com>
2024-08-26 23:50:11 +0800

    .

--- Misc.lua
+++ Misc.lua
@@ -19,3 +19,25 @@ do
 
 	hide()
 end
+
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.UnitAura, function(tooltip, data)
+	local mountID = data.id and C_MountJournal.GetMountFromSpell(data.id)
+	if not mountID then return end
+
+	local source = select(3, C_MountJournal.GetMountInfoExtraByID(mountID))
+	tooltip:AddLine(" ")
+	tooltip:AddLine(WHITE_FONT_COLOR:WrapTextInColorCode(source))
+end)
+
+for name, type in pairs(Enum.TooltipDataType) do
+	TooltipDataProcessor.AddTooltipPostCall(type, function(tooltip, data)
+		if not data.id or not IsShiftKeyDown() then
+			return
+		end
+
+		tooltip:AddLine(" ")
+		tooltip:AddDoubleLine(
+			WHITE_FONT_COLOR:WrapTextInColorCode(name .. " ID"),
+			WHITE_FONT_COLOR:WrapTextInColorCode(data.id))
+	end)
+end
