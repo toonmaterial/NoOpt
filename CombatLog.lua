618be2f11ff6f6a5fd5bc4fd094425c3204310da
toonmaterial <30956192+toonmaterial@users.noreply.github.com>
2024-09-14 19:18:32 +0800

    .

--- CombatLog.lua
+++ CombatLog.lua
@@ -1,5 +1,6 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("SCENARIO_UPDATE")
 f:SetScript("OnEvent", function()
 	if GetCVarBool("advancedCombatLogging") then
 		LoggingCombat(IsInInstance())
